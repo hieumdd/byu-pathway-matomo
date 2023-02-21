0e6ea3152a30d2218440c513879893eabdb9ec94
HM <hieumdd@gmail.com>
2023-02-21 23:01:28 +0700

    fix: Fix public_url

--- matomo/test_matomo_service.py
+++ matomo/test_matomo_service.py
@@ -5,7 +5,7 @@ from matomo import matomo_service
 
 TIMEFRAME = [
     ("auto", (None, None)),
-    ("manual", ("2020-01-01", "2022-07-15")),
+    # ("manual", ("2022-07-15", "2023-02-17")),
 ]
 
 
@@ -18,7 +18,7 @@ def timeframe(request):
 
 
 class TestMatomo:
-    @pytest.fixture(
+    @pytest.fixture( # type: ignore
         params=pipelines.values(),
         ids=pipelines.keys(),
     )
